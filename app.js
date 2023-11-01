280ce9ed1c7aa95d2ed18a7d02113a5b717d5dd8
umar musa <umargafia@gmail.com>
2023-11-01 12:00:59 +0100

    increase data limit

--- app.js
+++ app.js
@@ -31,7 +31,7 @@ const limiter = rateLimit({
 app.use('/api', limiter);
 app.use(cors());
 // Body parser, reading data from body into req.body
-app.use(express.json({ limit: '10kb' }));
+app.use(express.json({ limit: '1mb' }));
 
 // Data sanitization against NoSQL query injection
 app.use(mongoSanitize());
